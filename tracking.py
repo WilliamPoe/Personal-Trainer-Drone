8a4b07891b350da9e41f7d3e397c08035d50dc87
William Poe <williamtpoe@gmail.com>
2025-04-23 18:58:56 -0400

    Added file for tracking.

---
+++ tracking.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+
+class Tracker(Node):
+    def __init__(self):
+        super().__init__('tracking')
+
+        self.camera_sub = self.create_subscription(Image, '/bebop/camera/image_raw', self.listener_callback, 10)
+
+        self.bridge = CvBridge()
+
+    def listener_callback(self, msg):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            cv2.imshow('Camera Feed', cv_image)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f"Could not convert image: {e}")
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    drone_track = Tracker()
+
+    rclpy.spin(drone_track)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    drone_track.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
